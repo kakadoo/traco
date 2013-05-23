aad6d72399fa8f1fc3853cf71334894cc79398e0
Holger Glaess <holgerglaess@gmail.com>
2013-05-23 13:42:00 +0200

    fix for new Traco naming

--- contrib/test-hbanalyse.pl
+++ contrib/test-hbanalyse.pl
@@ -6,7 +6,7 @@ use Data::Dumper ;
 #my $file = '/opt/video.00/Universal_Soldier/2011-06-25.23.54.5-0.rec/vdrtranscode.ts';
 #my $file = '/opt/video.00/Avatar_-_Aufbruch_nach_Pandora/2012-04-08.20.13.5-0.rec/vdrtranscode.ts';
 
-my $file = '/opt/video.00/Elton_John:_Live_at_Ibiza123_feat._Pnau/2012-12-31.11.28.34-0.rec/vdrtranscode.ts';
+my $file = '/opt/video.00/Der_Rote_Baron/2013-05-19.22.48.6-0.rec/vdrtranscode.ts';
 
 my $traco = Traco::Traco->new();
 
