11c5d6c0f88644d9e4667fb31c73bbe3bf9e6166
Holger Glaess <holgerglaess@gmail.com>
2013-10-06 16:11:24 +0200

    add: some settings for vdrpsend

--- lib/Traco/Config.pm
+++ lib/Traco/Config.pm
@@ -8,12 +8,14 @@ use warnings;
 use English '-no_match_vars';
 use Carp;
 
+use Traco::Tracoio ;
+use Data::Dumper ;
+
 require Exporter;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-@EXPORT_OK = qw(parseconfig 
-						parse_config_value
-						);
+
+@EXPORT_OK = qw(parseconfig parse_config_value);
 
 use constant { FUENF => 5 };
 
@@ -23,31 +25,51 @@ sub new {
 	my ($class,$args) = @_;
 	my $config = {};
 	$class = ref($class) || $class;
-#	$config->{'facility'} = 'syslog';
-#	$confog->{'priority'} = 'info';
-
-	$config->{'configfile'} = '/etc/vdr/traco.conf' ;
-	$config->{'daemon_flag'} = '1';
-	$config->{'debug_flag'} = '0';
-	$config->{'interval'} = FUENF ;
-	$config->{'pidfile'} = '/var/run/vdr/tracosrv.pid';
-	$config->{'setcpu'} = 'auto';
-	$config->{'fpstype'} = 'vdr';
-	$config->{'traco_ts'} = 'vdrtranscode.ts';
-	$config->{'traco_xml'} = 'vdrtranscode.xml';
-	$config->{'traco_lck'} = 'vdrtranscode.lck';
-# set default profile 
-	$config->{'defaultprofile'} = 'SD';
-# nice default
-	$config->{'nice'} = '20';
-	$config->{'verbose_flag'} = q{};
+	$config -> {'configfile'} = '/etc/vdr/traco.conf' ;
+	$config -> {'daemon_flag'} = '1';
+	$config -> {'debug_flag'} = '0';
+	$config -> {'interval'} = FUENF ;
+	$config -> {'pidfile'} = '/var/run/vdr/tracosrv.pid';
+	$config -> {'setcpu'} = 'auto';
+	$config -> {'maxcpu'} = '2';
+	$config -> {'fpstype'} = 'vdr';
+	$config -> {'traco_ts'} = 'traco.ts';
+	$config -> {'traco_xml'} = 'traco.xml';
+	$config -> {'traco_lck'} = 'traco.lck';
+	$config -> {'traco_tmp'} = 'traco_tmp';
 	
+# set default profile 
+	$config -> {'defaultprofile'} = 'SD';
+# nice default
+	$config -> {'nice'} = '20';
+	$config -> {'verbose_flag'} = q{};
+	$config -> {'use_classic_profile'} = 'no' ;
 	$config -> {'facility'} = 'syslog';
 	$config -> {'priority'} = 'info' ;
 	$config -> {'writelog'} = q{};
 	$config -> {'vdr_user'} = 'vdr';
-
+	$config -> {'Indir'}  = '/video';
+	$config -> {'Outdir'}  = '/video';
+	
+	$config -> {'AAC_Bitrate' }  = '192' ;
+	$config -> {'DRC' } = '2.5' ; 
+	$config -> {'anamorph_encoding' } = '1';
+	$config -> {'filenameformat'} = '%t-%e(%d.%m.%y,%vxx%vy,%fps).%c';
+	$config -> {'writelog'}  = 'false' ;
+	$config -> {'usevdrfps'} = 'true' ; # should be in the future obsolete
+	$config -> {'fpstype'} = 'vdr' ; 
+	$config -> {'vdrversion'} = '1.7';
+	$config -> {'recalculate_bitrate' } = 'true' ;
+	$config -> {'debug_getfilelist' } = undef ;
+	$config -> {'svdrpsend_flags'} = '-d localhost -p 6419 -t 10';
+	
+	
 	bless $config,$class;
+
+	if ( -e $config->{'configfile'} ) { 
+		$config->parseconfig ({ config => $config->{'configfile'},debug => $config->{'debug'} });
+	}
+
 	return $config;
 } # end sub new
 
@@ -57,7 +79,7 @@ my ($self,$args) = @_;
 my $file = \$args->{'config'};
 my $debug = \$args->{'debug'};
 #my $config = {};
-my $lines = \$self->readfile({file=>${$file},});
+my $lines = \$self->Traco::Tracoio::readfile ({file => ${$file},});
 
 if ( ${$lines}->{'returncode'} !~ /[_]done$/smx ) {
   print {*STDOUT} "trouble to read configfile exit $PROGRAM_NAME\n" or croak $ERRNO ;
@@ -73,7 +95,7 @@ if ( ${$lines}->{'returncode'} !~ /[_]done$/smx ) {
     my ($key,$tmp_value) = split /\s*=(?:\s*|\t*)/smx ,$_,2;
     my $value = \$self->parse_config_value({value=>$tmp_value,debug=>${$debug},});
     undef $tmp_value;
-    if ( ${$debug} ) { print {*STDOUT} "[DEBUG] _parseconfig | $_\n" or croak $ERRNO; }
+    if ( ${$debug} ) { print {*STDOUT} "[DEBUG] parseconfig | $_\n" or croak $ERRNO; }
     $self->{$key} = ${$value};
   }
   undef $lines;
@@ -92,3 +114,4 @@ return ($rc);
 }
 
 1;
+
