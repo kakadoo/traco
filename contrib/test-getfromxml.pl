e9ee2266e9b94a01e57a43242db7740889296ca4
Holger Glaess <holgerglaess@gmail.com>
2013-10-08 12:12:11 +0200

    just testing

--- contrib/test-getfromxml.pl
+++ contrib/test-getfromxml.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/perl -w
+use lib '../lib';
 use Traco::Traco ;
 use Data::Dumper ;
 
-my $dir ='/opt/video.00/Fortress_2_-_Die_Festung/2011-05-05.23.49.6-0.rec';
+my $dir ='/opt/video.00/Queen:_Hungarian_Rhapsody_-_Live_in_Budapest/2013-09-14.20.13.21-0.rec';
 
 my $vdrtranscode = Traco::Traco->new();
 
-my $vdrinfo = \$vdrtranscode->getfromxml({file=>"$dir/vdrtranscode.xml",field=>'ALL',debug=>'1',});
+my $vdrinfo = \$vdrtranscode->getfromxml({file=>"$dir/traco.xml",block=>'vdrinfo',field=>'frames',debug=>'1',});
 
 
 print Dumper $vdrinfo;
