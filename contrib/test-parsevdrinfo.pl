c7be178def7f6fb77f3907d4b3b59dbe3b24188e
Holger Glaess <holgerglaess@gmail.com>
2013-12-05 08:44:04 +0100

    change to start direkt from contrib dir

--- contrib/test-parsevdrinfo.pl
+++ contrib/test-parsevdrinfo.pl
@@ -1,9 +1,10 @@
 #!/usr/bin/perl -w
-use lib 'lib/';
+use lib '../lib/';
 use Traco::Traco ;
 use Data::Dumper ;
 
-my $dir ='/opt/video.00/Der_Rote_Baron/2013-05-19.22.48.6-0.rec';
+my $dir ='/opt/video.00/1941_-_Wo,_bitte,_geht\'s_nach_Hollywood?/2013-10-11.02.22.10-0.rec';
+
 
 my $traco = Traco::Traco->new();
 
