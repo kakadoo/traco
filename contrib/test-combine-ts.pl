2b868eca37e411c12c8453d44197ee10bed56d4f
Holger Glaess <holgerglaess@gmail.com>
2014-03-11 15:57:08 +0100

    implement new options

--- contrib/test-combine-ts.pl
+++ contrib/test-combine-ts.pl
@@ -1,18 +1,27 @@
+use lib '../lib';
 use Traco::Traco;
+
 
 my $a=Traco::Traco->new({debug=>'1',});
 
 
 
-#my $dir ='/opt/video.00/Der_rosarote_Panther_2/2011-04-10.20.14.5-0.rec';
-#$dir = '/opt/video.00/The_Rolling_Stones#3A_Live_at_the_Max/2010-12-31.19.55.50.99.rec';
-#$dir = '/opt/video.00/Rockpalast/2010-06-21.01.09.51.99.rec';
-$dir='/opt/video.00/Blues_Brothers/2011-07-17.02.19.9-0.rec';
+$dir='/opt/video.00/Robbie_Williams_-_Let\'s_swing_again!/2013-12-07.23.38.1-0.rec';
 
 my $src='';
-my $dst="$dir/vdrtranscode.ts";
+my $dst='traco.ts';
 
-my $rc=\$a->combine_ts({source=>$dir,target=>$dst,vdrversion=>'1.7',debug=>'1',f=>'25',});
+my $rc=\$a->combine_ts({source=>$dir,
+			target=>$dst,
+			vdrversion=>'1.7',
+			debug=>'1',
+			fpstype=>'vdr',
+			nice=>'nice -n 20',
+			handbrake=>'/usr/bin/HandBrakeCLI',
+			marksfile=>"$dir/marks",
+			xml => 'traco.xml',
+			indexfile => 'index',
+});
 
 print "test ergebnis ${$rc}\n";
 
