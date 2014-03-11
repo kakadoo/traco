71ee9d584f97c1cf9eb83dc71e7fce4c47c73b1c
Holger Glaess <holgerglaess@gmail.com>
2014-03-11 15:57:48 +0100

    add new options

--- contrib/test-joinfiles.pl
+++ contrib/test-joinfiles.pl
@@ -1,14 +1,14 @@
-use lib 'lib/';
+use lib '../lib/';
 use Traco::Traco;
 
 my $a=Traco::Traco->new({debug=>'1',});
 
 
 
-my $dir = '/opt/video.00/Bernhard_Victor_Christoph_Carl_von_Bülow/2011-08-23.22.00.3-0.rec';
+my $dir = '/opt/video.00/Robbie_Williams_-_Let\'s_swing_again!/2013-12-07.23.38.1-0.rec';
 my $src='';
 
-my $rc=\$a->_joinfiles({dir=>$dir,files=>'00001.ts 00002.ts',debug=>'1',f=>'25',});
+my $rc=\$a->_joinfiles({dir=>$dir,files=>'00001.ts 00002.ts 00003.ts 00004.ts',debug=>'1',destination=>"$dir/traco.ts"});
 
 print "test ergebnis ${$rc}\n";
 
